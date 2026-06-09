757335f98b2c187c461fa8178c91130411b79daa
Dictator <dictatorbogdan@gmail.com>
2026-06-09 14:16:56 +0300

    fix schema fields validation and empty strings

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -17,15 +17,15 @@ export const getAllNotesSchema = {
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
-    search: Joi.string(),
+    search: Joi.string().allow(''),
   }),
 };
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string(),
-    tag: Joi.valid(...TAGS),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
   }),
 };
 
