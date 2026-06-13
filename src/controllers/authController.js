39089be14335651846d9d62678089b2fff4737aa
Dictator <dictatorbogdan@gmail.com>
2026-06-13 12:43:09 +0300

    add password hashing and user creation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,22 @@
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import User from '../models/user.js';
 
 export const registerUser = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw createHttpError(400, 'Email in use');
   }
-  const newUser = await User.create(req.body);
-  console.log(newUser.toJSON);
+  // Хешуємо пароль
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  // Створюємо користувача
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+  });
+
+  // Відправляємо дані користувача (без пароля) у відповіді
   res.status(201).json(newUser);
 };
