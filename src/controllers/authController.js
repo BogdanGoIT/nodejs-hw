47d5118401ccbc495c0953338b1e2a8dea396a00
Dictator <dictatorbogdan@gmail.com>
2026-06-14 12:21:15 +0300

    add session creation and cookie helpers in auth service

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import User from '../models/user.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+import Session from '../models/session.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -16,6 +18,9 @@ export const registerUser = async (req, res) => {
     ...req.body,
     password: hashPassword,
   });
+
+  const session = createSession(newUser._id);
+  setSessionCookies(res, session);
 
   // Відправляємо дані користувача (без пароля) у відповіді
   res.status(201).json(newUser);
@@ -36,6 +41,11 @@ export const loginUser = async (req, res) => {
   if (!isValidPassword) {
     throw createHttpError(401, 'Invalid credentials');
   }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = createSession(user._id);
+  setSessionCookies(res, session);
 
   res.status(200).json(user);
 };
