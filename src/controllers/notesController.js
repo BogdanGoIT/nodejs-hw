28fee796ed95d016b6d7bf42785a9fcc7dee38d3
Dictator <dictatorbogdan@gmail.com>
2026-06-16 12:49:41 +0300

    link notes to users

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -4,10 +4,14 @@ import { Note } from '../models/note.js';
 
 // Отримати список усіх нотаток
 export const getAllNotes = async (req, res) => {
+  const { _id: userId } = req.user;
   const { page = 1, perPage = 10, tag, search } = req.query;
   console.log(search, tag);
   const skip = (page - 1) * perPage;
   const notesQuery = Note.find();
+  if (userId) {
+    notesQuery.where('userId').equals(userId);
+  }
   if (tag) {
     notesQuery.where('tag').equals(tag);
   }
@@ -35,7 +39,8 @@ export const getAllNotes = async (req, res) => {
 // Отримати одну нотатку за id
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId); // throw new Error()
+  const { _id: userId } = req.user;
+  const note = await Note.findOne({ _id: noteId, userId }); // throw new Error()
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -60,8 +65,10 @@ export const createNote = async (req, res) => {
 // видалення існуючої нотатки за її ідентифікатором
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
+  const { _id: userId } = req.user;
   const deleteNote = await Note.findOneAndDelete({
     _id: noteId,
+    userId,
   });
 
   if (!deleteNote) {
@@ -74,9 +81,10 @@ export const deleteNote = async (req, res) => {
 // оновлення існуючої нотатки за її ідентифікатором
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
+  const { _id: userId } = req.user;
 
   const updateNote = await Note.findOneAndUpdate(
-    { _id: noteId }, // Шукаємо по id
+    { _id: noteId, userId }, // Шукаємо по id
     req.body,
     { returnDocument: 'after', runValidators: true }, // повертаємо оновлений документ
   );
